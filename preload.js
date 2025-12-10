d27459d44dba707c4a4e1d0cfff595cb258aec96
kim yongjin <Yongjin.dev@gmail.com>
2025-12-10 20:28:34 +0900

    Add multi-image carousel feature with per-image crop position

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  savePostImage: (postId, imageData) => ipcRenderer.invoke('save-post-image', postId, imageData),
+  savePostImage: (postId, imageData, mode = 'replace') => ipcRenderer.invoke('save-post-image', postId, imageData, mode),
   toggleLike: (postId) => ipcRenderer.invoke('toggle-like', postId),
-  updateCropPosition: (postId, cropY) => ipcRenderer.invoke('update-crop-position', postId, cropY),
+  updateCropPosition: (postId, cropY, imageIndex = 0) => ipcRenderer.invoke('update-crop-position', postId, cropY, imageIndex),
   deletePost: (postId) => ipcRenderer.invoke('delete-post', postId)
 })
